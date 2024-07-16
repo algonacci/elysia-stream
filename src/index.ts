e6bbbb0331caa98ed2aaa51cb77daeb124834326
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2024-07-16 15:30:35 +0700

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import { Elysia, t } from "elysia";
+import { Stream } from '@elysiajs/stream'
+import { OpenAI } from "openai"
+import cors from "@elysiajs/cors";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
+})
+
+const app = new Elysia()
+  .use(cors())
+  .get('/', () => new Stream(async (stream) => {
+    stream.send('hello')
+
+    await stream.wait(1000)
+    stream.send('world')
+
+    stream.close()
+  }))
+  .post(
+    '/ai',
+    ({ body }) => {
+      const { prompt } = body
+      return new Stream(
+        openai.chat.completions.create({
+          model: 'gpt-3.5-turbo',
+          stream: true,
+          messages: [
+            { "role": "system", "content": "You are a helpful assistant." },
+            { "role": "user", "content": `${prompt}` }
+          ],
+        })
+      )
+    }, {
+    body: t.Object({
+      prompt: t.String()
+    })
+  }
+  )
+  .listen(3000)
+
+
+console.log(
+  `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
+);
