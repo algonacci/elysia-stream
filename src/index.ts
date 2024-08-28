40821d43d068923430b0c7c08d83dae123bdfd57
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2024-08-28 22:44:09 +0700

    huehehe

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,48 @@
 import { Elysia, t } from "elysia";
-import { Stream } from '@elysiajs/stream'
-import { OpenAI } from "openai"
+import { Stream } from "@elysiajs/stream";
+import { OpenAI } from "openai";
 import cors from "@elysiajs/cors";
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY
-})
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 const app = new Elysia()
   .use(cors())
-  .get('/', () => new Stream(async (stream) => {
-    stream.send('hello')
+  .get(
+    "/",
+    () =>
+      new Stream(async (stream) => {
+        stream.send("hello");
 
-    await stream.wait(1000)
-    stream.send('world')
+        await stream.wait(1000);
+        stream.send("world");
 
-    stream.close()
-  }))
+        stream.close();
+      })
+  )
   .post(
-    '/ai',
+    "/ai",
     ({ body }) => {
-      const { prompt } = body
+      const { prompt } = body;
       return new Stream(
         openai.chat.completions.create({
-          model: 'gpt-3.5-turbo',
+          model: "gpt-3.5-turbo",
           stream: true,
           messages: [
-            { "role": "system", "content": "You are a helpful assistant." },
-            { "role": "user", "content": `${prompt}` }
+            { role: "system", content: "You are a helpful assistant." },
+            { role: "user", content: `Return dalam bentuk Markdown ${prompt}` },
           ],
         })
-      )
-    }, {
-    body: t.Object({
-      prompt: t.String()
-    })
-  }
+      );
+    },
+    {
+      body: t.Object({
+        prompt: t.String(),
+      }),
+    }
   )
-  .listen(3000)
-
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
